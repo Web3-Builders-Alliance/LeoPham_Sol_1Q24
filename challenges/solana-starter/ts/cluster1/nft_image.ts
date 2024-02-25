f59812b1e99824b99575d81574f9aae4cb758e5d
HongThaiPham <hongthaipro@gmail.com>
2024-02-24 21:23:42 -0800

    Done challenge 3

--- challenges/solana-starter/ts/cluster1/nft_image.ts
+++ challenges/solana-starter/ts/cluster1/nft_image.ts
@@ -1,11 +1,15 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
-import { readFile } from "fs/promises"
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
@@ -14,17 +18,22 @@ umi.use(irysUploader());
 umi.use(signerIdentity(signer));
 
 (async () => {
-    try {
-        //1. Load image
-        //2. Convert image to generic file.
-        //3. Upload image
+  try {
+    //1. Load image
+    //2. Convert image to generic file.
+    //3. Upload image
 
-        // const image = ???
+    const image = await readFile(
+      "/Users/leo/Desktop/wba/LeoPham_Sol_1Q24/challenges/solana-starter/ts/images/generug.png"
+    );
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const [myUri] = await umi.uploader.upload([
+      createGenericFile(image, "generug", {
+        contentType: "image/png",
+      }),
+    ]);
+    console.log("Your image sURI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
--- challenges/solana-starter/ts/cluster1/nft_metadata.ts
+++ challenges/solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,45 +1,51 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
-const bundlrUploader = createBundlrUploader(umi);
+const umi = createUmi("https://api.devnet.solana.com");
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
 umi.use(signerIdentity(signer));
-
+umi.use(irysUploader());
 (async () => {
-    try {
-        // Follow this JSON structure
-        // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+  try {
+    // Follow this JSON structure
+    // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const image =
+      "https://arweave.net/Fs6IK_ooQ0_P1XZ5t_XKEJC7QgIWG-PPsu0DzreLIKo";
+    const metadata = {
+      name: "Leo Rug",
+      symbol: "LRT",
+      description: "A generative rug",
+      image: image,
+      attributes: [
+        { trait_type: "Color", value: "RGB" },
+        { trait_type: "Size", value: "Large" },
+        { trait_type: "Material", value: "Digital" },
+        { trait_type: "Pattern", value: "Generative" },
+      ],
+      properties: {
+        files: [
+          {
+            type: "image/png",
+            uri: image,
+          },
+        ],
+      },
+      creators: [],
+    };
+    const myUri = await umi.uploader.uploadJson(metadata);
+    console.log("Your image URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
