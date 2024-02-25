91f748f92a5e51d93bc70232f9301159d5b87675
HongThaiPham <hongthaipro@gmail.com>
2024-02-24 20:55:58 -0800

    add prereq

---
+++ prerequisites/keygen.ts
@@ -0,0 +1,7 @@
+import { Keypair } from "@solana/web3.js";
+import bs58 from "bs58";
+
+let kp = Keypair.generate();
+
+console.log(`You've generated a new Solana wallet:
+${kp.publicKey.toBase58()} [${kp.secretKey}]`);
